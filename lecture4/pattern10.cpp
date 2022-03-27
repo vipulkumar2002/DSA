b9498dc8d4915af24c26121ead2e1e2d57f876fd
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-03-28 02:08:28 +0530

    modified pattern10

--- lecture4/pattern10.cpp
+++ lecture4/pattern10.cpp
@@ -14,12 +14,22 @@ int main()
         int j = 1;
         while (j <= n)
         {
-            char result = 'A';
+            char result = 'A' + i + j - 2;
             cout << result << " ";
-            result + i + j - 1;
             j++;
         }
         i++;
         cout << endl;
     }
 }
+
+/*
+Output
+
+A B C D E
+B C D E F
+C D E F G
+D E F G H
+E F G H I
+
+*/
