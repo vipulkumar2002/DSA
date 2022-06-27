e16bc9ee520e84cfcb34406f414b7a05c9678cb4
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-06-27 17:08:22 +0530

    peck in mountain

---
+++ L-12 Binary_Search/peckInMountain.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int find_pivot(int arr[], int size)
+{
+    int s = 0, e = size - 1;
+
+    int mid = s + (e - s) / 2;
+
+    while (s < e)
+    {
+        if (arr[mid] < arr[mid + 1])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+
+        mid = s + (e - s) / 2;
+    }
+    return s;
+}
+
+int main()
+{
+    int arr[4] = {0, 10, 5, 2};
+
+    cout << "pivot element is : " << find_pivot(arr, 4);
+}
