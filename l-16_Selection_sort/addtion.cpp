8f68fdcbafb259ca9eef53f8c2117eb45684a3de
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-07-12 13:23:42 +0530

    collage trainnig

---
+++ l-16_Selection_sort/addtion.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int n1, n2;
+    // int n;
+
+    // cout << "Enter two number : " << endl;
+
+    // cin >> n1 >> n2;
+
+    // cout << "enter op";
+    // cin >> n;
+    // switch (n)
+    // {
+    // case 1:
+    //     break;
+    // case 2:
+    //     break;
+    // case 3:
+    //     break;
+    // case 4:
+    //     break;
+    // default:
+    // {
+
+    //     cout << "Enter Valid chose : ";
+    //     exit(0);
+    // }
+    //     return 0;
+    // }
+
+    //================================================
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << "abcd";
+    //     break;
+    // }
+
+    // int i;
+
+    // for (i = 0; i < 3; i++)
+    // {
+    //     int a = 5;
+    //     int b = 6;
+    // }
+
+    // cout << i << endl;
+    // return 0;
+
+    //===============================================================
+
+    // int n;
+    // cout << "enter number to add : ";
+    // cin >> n;
+    // int sum = 0;
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     sum += i;
+    // }
+
+    // cout << "Sum of n number : " << sum;
+    // return 0;
+
+    // ====================================================================
+
+    // int n;
+    // cin >> n;
+    // int count = 0;
+
+    // while (n > 0)
+    // {
+    //     count += n % 10;
+
+    //          n = n / 10;
+    // }
+    // cout << count;
+
+    // =======================================================================
+    /* int n;
+
+    cin >> n;
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+
+        if (n == 0)
+        {
+            fact = 1;
+        }
+        else if (n < 0)
+        {
+            cout << "Enterd no is not valid : ";
+        }
+        else
+        {
+
+            fact = fact * i;
+        }
+    }
+
+    cout << fact; */
+
+    // =============================================================
+
+    /*   int long long n;
+
+      cin >> n;
+      int long long digit;
+      while (n != 0)
+      {
+          digit = n % 10;
+          cout << digit;
+          n = n / 10;
+      } */
+
+    // ==============================================================
+
+    return 0;
+}
---
+++ l-16_Selection_sort/selection_sort.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int y = (1, 2);
+    int z;
+    z = 3, 4;
+    cout << y << endl;
+    cout << z;
+
+    return 0;
+}
