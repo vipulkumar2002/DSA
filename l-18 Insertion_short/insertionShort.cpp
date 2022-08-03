6f077ff14f016c0c88beea220855edecca01f692
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-08-03 13:13:40 +0530

    insertion short done

--- l-18 Insertion_short/insertionShort.cpp
+++ l-18 Insertion_short/insertionShort.cpp
@@ -3,8 +3,24 @@ using namespace std;
 
 void insertionShort(int arr[], int n)
 {
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 1; i < n; i++)
     {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
 }
 
@@ -12,17 +28,18 @@ int main()
 {
 
     int n;
-    cout << 'Enter the size of array: ' << endl;
+    cout << "Enter the size of array: " << endl;
     cin >> n;
 
     int arr[n];
-    cout << 'Enter the array elements :' << endl;
+    cout << "Enter the array elements :" << endl;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
     insertionShort(arr, n);
+    print(arr, n);
 
     return 0;
 }
