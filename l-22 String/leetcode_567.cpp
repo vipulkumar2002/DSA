37a949631669ccd349722c3fef35acb44ddc9977
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-08-17 01:23:32 +0530

    codeStudio 54

--- l-22 String/leetcode_567.cpp
+++ l-22 String/leetcode_567.cpp
@@ -10,25 +10,34 @@ using namespace std;
 
 bool checkInclusion(string s1, string s2)
 {
+
+    int sumS1 = 0;
+    for (int i = 0; i < s1.length(); i++)
+    {
+        sumS1 = sumS1 + s1[i] - 'a' + 1;
+    }
+    cout << sumS1 << endl;
+
     for (int i = 0; i < s2.length(); i++)
     {
-        if (s2.find(s1) || s2.rfind(s1))
+        int sumS2 = 0;
+        for (int j = i; j < i + 2; j++)
         {
-            return true;
+            sumS2 = sumS2 + s2[j] - 'a' + 1;
         }
-        else
+        if (sumS1 == sumS2)
         {
-            return false;
+            return 1;
         }
     }
+    return 0;
 }
 
 int main()
 {
-    string s1 = "ab";
-    string s2 = "eidboaoo";
+    string s1 = "ac";
+    string s2 = "eidbaooo";
+
     cout << checkInclusion(s1, s2);
     return 0;
 }
-
-not solved !
